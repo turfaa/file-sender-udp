47f452f4c08d12725b24e35c6e4af6d6dadf56dc
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2017-10-16 16:46:19 +0700

    change checksum type

--- checksum.h
+++ checksum.h
@@ -1,8 +1,8 @@
 #ifndef _CHECKSUM_H
 #define _CHECKSUM_H
 
-unsigned short checksum(char * data, unsigned short size) {
-    unsigned short checksum_result = 0;
+char checksum(char * data, unsigned short size) {
+    char checksum_result = 0;
 
     for(unsigned short i = 0; i < size; i++) {
         checksum_result += data[i];
